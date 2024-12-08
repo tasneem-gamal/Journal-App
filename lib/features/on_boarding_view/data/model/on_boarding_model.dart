e7de0aaf4e743f6a00650e184b6266435477416e
tasneem-gamal <tasneemgamal044@gmail.com>
2024-12-08 13:05:47 -0800

    adding styles, colors , screenutil, getx

---
+++ lib/features/on_boarding_view/data/model/on_boarding_model.dart
@@ -0,0 +1,7 @@
+class OnBoardingModel {
+  String? image;
+  String? title;
+  String? subTitle;
+
+  OnBoardingModel({this.image, this.subTitle, this.title});
+}
--- lib/features/on_boarding_view/ui/widgets/on_boarding_view_body.dart
+++ lib/features/on_boarding_view/ui/widgets/on_boarding_view_body.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:journal_app/core/helper_functions/screen_size_methods.dart';
+import 'package:journal_app/core/theming/images_constants.dart';
+import 'package:journal_app/core/theming/styles.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
   const OnBoardingViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Stack(
+      children: [
+        PageView(
+          children: [
+            Column(
+              children: [
+                Image(
+                  height: ScreenSizeMethods.screenHeight(context) * 0.6,
+                  width: ScreenSizeMethods.screenWidth(context) * 0.8,
+                  image: const AssetImage(ImagesConstants.onBoardingOne)
+                ),
+                Text(
+                  'Capture your thoughts, one entry at a time.',
+                  style: Styles.textStyle16Medium,
+                )
+              ],
+            )
+          ],
+        )
+      ],
+    );
   }
 }
