aa26442adb41159480069fdb351fe6c50af25896
tasneem-gamal <tasneemgamal044@gmail.com>
2024-12-08 11:47:15 -0800

    init

---
+++ lib/journal_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class JournalApp extends StatelessWidget {
+  const JournalApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
